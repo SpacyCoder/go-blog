515d39a27e1eff721ff81406fe217e9d8db83529
SpacyCoder <sral_h@hotmail.com>
2018-11-02 20:07:40 +0100

    update bash script

--- copyall.sh
+++ copyall.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+export GOPATH=/home/spacy/go
 export GOOS=linux
 export CGO_ENABLED=0
 cd accountservice;go get;go build -o accountservice-linux-amd64;echo built `pwd`;cd ..
+
+cd healthchecker;go get; go build -o healthchecker-linux-amd64;echo built `pwd`;cd ..
+
 export GOOS=darwin
-docker build -t spacycoder/accountservice accountservice/
-docker service rm accountservice
-docker service create --name=accountservice --replicas=1 --network=my_network -p=6767:6767 someprefix/accountservice
+
+cp healthchecker/healthchecker-linux-amd64 accountservice/
+
+sudo docker build -t spacycoder/accountservice accountservice/
+sudo docker service rm accountservice
+sudo docker service create --name=accountservice --replicas=1 --network=my_network -p=6767:6767 spacycoder/accountservice
