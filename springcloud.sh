3cb55353aff32b2618f38bd36bd212131e7b0ba2
SpacyCoder <sral_h@hotmail.com>
2018-11-03 16:57:12 +0100

    add turbine

--- springcloud.sh
+++ springcloud.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
+# Config Server
 cd support/config-server
 ./gradlew build
 cd ../..
 docker build -t spacycoder/configserver support/config-server/
 docker service rm configserver
 docker service create --replicas 1 --name configserver -p 8888:8888 --network my_network --update-delay 10s --with-registry-auth  --update-parallelism 1 spacycoder/configserver
+
+# Hystrix Dashboard
+docker build -t spacycoder/hystrix support/monitor-dashboard
+docker service rm hystrix
+docker service create --constraint node.role==manager --replicas 1 -p 7979:7979 --name hystrix --network my_network --update-delay 10s --with-registry-auth  --update-parallelism 1 spacycoder/hystrix
+
+# Turbine
+docker service rm turbine
+docker service create --constraint node.role==manager --replicas 1 -p 8282:8282 --name turbine --network my_network --update-delay 10s --with-registry-auth  --update-parallelism 1 eriklupander/turbine
