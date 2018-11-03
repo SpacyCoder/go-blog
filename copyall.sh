17590b8bebecf760dc54be2b3ec53625710e73a5
SpacyCoder <sral_h@hotmail.com>
2018-11-03 15:18:07 +0100

    edit

--- copyall.sh
+++ copyall.sh
@@ -4,23 +4,17 @@ export GOOS=linux
 export CGO_ENABLED=0
 
 cd accountservice;go get;go build -o accountservice-linux-amd64;echo built `pwd`;cd ..
-cd vipservice;go get;go build -o vipservice-linux-amd64;echo built `pwd`;cd ..
 cd healthchecker;go get;go build -o healthchecker-linux-amd64;echo built `pwd`;cd ..
 cd imageservice;go get;go build -o imageservice-linux-amd64;echo built `pwd`;cd ..
 
 export GOOS=darwin
 
 cp healthchecker/healthchecker-linux-amd64 accountservice/
-cp healthchecker/healthchecker-linux-amd64 vipservice/
 cp healthchecker/healthchecker-linux-amd64 imageservice/
 
 docker build -t spacycoder/accountservice accountservice/
 docker service rm accountservice
 docker service create --log-driver=gelf --log-opt gelf-address=udp://192.168.99.100:12202 --log-opt gelf-compression-type=none --name=accountservice --replicas=1 --network=my_network -p=6767:6767 spacycoder/accountservice
-
-docker build -t spacycoder/vipservice vipservice/
-docker service rm vipservice
-docker service create --log-driver=gelf --log-opt gelf-address=udp://192.168.99.100:12202 --log-opt gelf-compression-type=none --name=vipservice --replicas=1 --network=my_network -p=6868:6868 spacycoder/vipservice
 
 docker build -t spacycoder/imageservice imageservice/
 docker service rm imageservice
