0bee51912ec3cf2300410dbd6b3d2d8e2a1da2bb
SpacyCoder <sral_h@hotmail.com>
2018-11-03 11:41:21 +0100

    add  config loader

--- accountservice/main.go
+++ accountservice/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
+	"github.com/callistaenterprise/goblog/accountservice/config"
 	"github.com/spacycoder/go-blog/accountservice/dbclient"
 	"github.com/spacycoder/go-blog/accountservice/service"
+	"github.com/spf13/viper"
 )
 
 var appName = "accountservice"
 
+func init() {
+	profile := flag.String("profile", "test", "Environment profile, something similar to spring profiles")
+	configServerURL := flag.String("configServerUrl", "http://configserver:8888", "Address to config server")
+	configBranch := flag.String("configBranch", "master", "git branch to fetch configuration from")
+	flag.Parse()
+
+	viper.Set("profile", *profile)
+	viper.Set("configServerURL", *configServerURL)
+	viper.Set("configBranch", *configBranch)
+}
+
 func main() {
 
 	fmt.Printf("Starting %v\n", appName)
+
+	config.LoadConfigurationFromBranch(viper.GetString("configServerURL"), appName, viper.GetString("profile"), viper.GetString("configBranch"))
 	initializeBoltClient()
-	service.StartWebServer("6767")
+	service.StartWebServer(viper.GetString("server_port"))
 
 }
 
